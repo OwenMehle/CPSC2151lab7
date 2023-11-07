0cfd0e9d890076e2c6b2d12b0ea25e1f34310fdc
OwenMehle <143368653+OwenMehle@users.noreply.github.com>
2023-11-07 10:49:46 -0500

    Update IShuffleList.java

---
+++ ShuffleList/src/cpsc2150.listDec/IShuffleList.java
@@ -0,0 +1,20 @@
+package cpsc2150.listDec;
+import java.util.List;
+import java.util.Random;
+
+public interface IShuffleList <T> extends List <T> {
+    default void shuffle(int swaps){
+        Random rand = new Random();
+        int rand1 = rand.nextInt();
+        int rand2 = rand.nextInt();
+        for(int i=0; i <swaps; i++){
+            swap(rand1, rand2);
+            rand1 = rand.nextInt();
+            rand2 = rand.nextInt();
+        }
+        
+    }
+    default void swap(int i, int j){
+
+    }
+}
