aa560f37525444022defad97b8e90c89a389a8e8
coal719 <cashb719@gmail.com>
2023-11-07 13:04:59 -0500

    stuff

--- ShuffleList/src/cpsc2150.listDec/IShuffleList.java
+++ ShuffleList/src/cpsc2150.listDec/IShuffleList.java
@@ -3,18 +3,19 @@ import java.util.List;
 import java.util.Random;
 
 public interface IShuffleList <T> extends List <T> {
-    default void shuffle(int swaps){
+    default void shuffle(int swaps) {
         Random rand = new Random();
-        int rand1 = rand.nextInt();
-        int rand2 = rand.nextInt();
-        for(int i=0; i <swaps; i++){
+        int rand1 = rand.nextInt(myList.sizeOf());
+        int rand2 = rand.nextInt(myList.sizeOf());
+        for (int i = 0; i < swaps; i++) {
             swap(rand1, rand2);
-            rand1 = rand.nextInt();
-            rand2 = rand.nextInt();
+            rand1 = rand.nextInt(myList.sizeOf());
+            rand2 = rand.nextInt(myList.sizeOf());
         }
-        
     }
     default void swap(int i, int j){
-
+        T temp = myList.get(i);
+        myList.set(i, j);
+        myList.set(temp, j);
     }
 }
